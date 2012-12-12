bdd680d4c051278cd8f157f962569c8eaacf8b4c
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-12 15:52:27 -0800

    Modified the RampancyCompiler so that there weren't redundant includes

--- lib/rampancy/RampancyCompiler.cpp
+++ lib/rampancy/RampancyCompiler.cpp
@@ -1,34 +1,8 @@
-#include "clang/CodeGen/CodeGenAction.h"
-#include "clang/Driver/Compilation.h"
-#include "clang/Driver/Driver.h"
-#include "clang/Driver/Tool.h"
-#include "clang/Frontend/CompilerInvocation.h"
-#include "clang/Frontend/CompilerInstance.h"
-#include "clang/Frontend/DiagnosticOptions.h"
-#include "clang/Frontend/FrontendDiagnostic.h"
-#include "clang/Frontend/TextDiagnosticPrinter.h"
-
-#include "llvm/Module.h"
-#include "llvm/ADT/OwningPtr.h"
-#include "llvm/ADT/SmallString.h"
-#include "llvm/ExecutionEngine/JIT.h"
-#include "llvm/ExecutionEngine/ExecutionEngine.h"
-#include "llvm/Support/ManagedStatic.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/Host.h"
-#include "llvm/Support/Path.h"
-#include "llvm/Support/TargetSelect.h"
-
-#include "ExpertSystem/KnowledgeConstructor.h"
-#include "ExpertSystem/FunctionNamer.h"
-#include "ExpertSystem/KnowledgeConstructionEngine.h"
 #include "rampancy/RampancyCompiler.h"
 
 extern "C" {
 #include "clips.h"
 }
-using namespace clang;
-using namespace clang::driver;
 
 namespace rampancy {
    llvm::sys::Path getExecutablePath(const char* argv0) {
