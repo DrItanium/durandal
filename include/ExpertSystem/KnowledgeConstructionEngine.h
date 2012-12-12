003a17a3e5c5cab009c2d50349bddf8759639730
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-11 18:13:03 -0800

    Moved the ExpertSystem headers into a subfolder (ExpertSystem)
    This is done to make the code far easier to manage and extend
    
    I also added a Passes folder for temporary usage

---
+++ include/ExpertSystem/KnowledgeConstructionEngine.h
@@ -0,0 +1,14 @@
+#ifndef _knowledge_construction_engine_h
+#define _knowledge_construction_engine_h
+
+#include "CLIPSArgumentBuilder.h"
+#include "CLIPSBasicBlockBulder.h"
+#include "CLIPSConstantBuilder.h"
+#include "CLIPSEnvironmentWrapper.h"
+#include "CLIPSInstructionBuilder.h"
+#include "CLIPSLoopBuilder.h"
+#include "CLIPSObjectBuilder.h"
+#include "CLIPSOperatorBuilder.h"
+#include "CLIPSRegionBuilder.h"
+#include "CLIPSTypeBuilder.h"
+#endif
