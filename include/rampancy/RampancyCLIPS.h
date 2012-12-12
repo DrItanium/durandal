b36ff8612d960bfbfdee5cb63a440f71a90891ab
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-11 21:01:55 -0800

    Added a header that contains the interface between Rampancy and CLIPS
    Rampancy is the code name for the interface durandal has into clang.
    It allows clips to dynamically compile a file without having to use a special
    pass to do it.

---
+++ include/rampancy/RampancyCLIPS.h
@@ -0,0 +1,7 @@
+#ifndef _rampancy_llvm_h
+#define _rampancy_llvm_h
+// Contains the header interface between the Rampancy library and 
+// clips. 
+
+extern "C" SetupRampancyExpertSystemInterfaces(void* ptr);
+#endif
