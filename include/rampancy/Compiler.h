4db2bd5ef1fda32c0dfc257443746a579631b149
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-13 17:17:11 -0800

    Removed the straight void* compile and interpret methods
    They were causing tons compiler errors due to the use of the void*

--- include/rampancy/Compiler.h
+++ include/rampancy/Compiler.h
@@ -6,12 +6,11 @@
  *
  * This class is abstract
  */
-#include <vector.h>
+#include <vector>
 #include <string.h>
 #include "llvm/Pass.h"
 #include "llvm/Module.h"
 #include "ExpertSystem/CLIPSEnvironment.h"
-#include "rampancy/AnalysisUsage.h"
 
 namespace rampancy {
    class Compiler : public llvm::ModulePass  {
@@ -19,14 +18,10 @@ namespace rampancy {
       public:
          Compiler(char ID);
          virtual ~Compiler();
-         virtual llvm::Module* compile(void* theEnv, int argc, char** argv) = 0;
-         virtual llvm::Module* compile(void* theEnv, std::vector<std::string> args) = 0;
-         virtual llvm::Module* interpret(void* theEnv, char* input) = 0;
-         virtual llvm::Module* interpret(void* theEnv, std::string input) = 0;
-         virtual llvm::Module* compile(CLIPSEnvironment* theEnv, int argc, char** argv);
-         virtual llvm::Module* compile(CLIPSEnvironment* theEnv, std::vector<std::string> args);
-         virtual llvm::Module* interpret(CLIPSEnvironment* theEnv, char* input);
-         virtual llvm::Module* interpret(CLIPSEnvironment* theEnv, std::string input);
+         virtual llvm::Module* compile(CLIPSEnvironment* theEnv, int argc, char** argv) = 0;
+         virtual llvm::Module* compile(CLIPSEnvironment* theEnv, std::vector<std::string> args) = 0;
+         virtual llvm::Module* interpret(CLIPSEnvironment* theEnv, char* input) = 0;
+         virtual llvm::Module* interpret(CLIPSEnvironment* theEnv, std::string input) = 0;
          virtual void getAnalysisUsage(llvm::AnalysisUsage& info);
          virtual void beforeKnowledgeConstruction(llvm::Module* module, CLIPSEnvironment* theEnv);
          virtual void afterKnowledgeConstruction(llvm::Module* module, CLIPSEnvironment* theEnv);
