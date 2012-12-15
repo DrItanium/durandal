3e7f16b282d9ca17d4f9694543e2d707cb629792
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-14 22:19:51 -0800

    Finished updating the ClangCompiler.

--- lib/rampancy/RampancyClangCompiler.cpp
+++ lib/rampancy/RampancyClangCompiler.cpp
@@ -5,49 +5,128 @@ extern "C" {
 }
 
 namespace rampancy {
+#define msg(a) (char*) a   
    llvm::sys::Path getExecutablePath(const char* argv0) {
       void *MainAddr = (void*) (intptr_t) getExecutablePath;
       return llvm::sys::Path::GetMainExecutable(argv0, MainAddr);
    }
-   ClangCompiler::ClangCompiler(CLIPSEnvironment& e, char* const *ep) {
-      envp = ep;
-      env = &e;
-      builder = new KnowledgeConstructor;
+   const char* ClangCompiler::getArgv0() {
+      return argv0;
    }
-   ClangCompiler::~ClangCompiler() {
-      if(builder) delete builder;
+   void ClangCompiler::setArgv0(const char* arg0) {
+      argv0 = arg0;
    }
-
-   void ClangCompiler::resetKnowledgeBuilder() {
-      builder->resetInstanceStream();
-   }
-   int ClangCompiler::execute(llvm::Module* mod, std::vector<std::string>& args, char* functionName) {
-      llvm::InitializeNativeTarget();
-
-      std::string error;
-      std::vector<std::string>::iterator it;
-      it = args.begin();
-      OwningPtr<llvm::ExecutionEngine> ee(
-            llvm::ExecutionEngine::createJIT(mod, &error));
-      if(!ee) {
-         llvm::errs() << "unable to make execution engine: " << error << '\n';
-         return 255;
+   ClangCompiler::ClangCompiler() : Compiler(ID) { }
+   llvm::Module* ClangCompiler::compile() {
+      void* theEnv = getEnvironment()->getEnvironment();
+      DATA_OBJECT multifield;
+      //if we've gotten this far then it's safe to say the routing was
+      //successful
+      if(!EnvArgTypeCheck(theEnv, msg("compile"), 2, MULTIFIELD, 
+               &multifield)) {
+         EnvPrintRouter(theEnv, msg("werror"),
+               msg("\nThe second argument must be a multifield!\n"));
+         return 0;
       }
-      llvm::Function* entryFunction = mod->getFunction(functionName);
-      if(!entryFunction) {
-         llvm::errs() << functionName << " function not found in module.\n";
-         return 255;
+      //if we don't have any args then just kick back
+      if(GetDOLength(multifield) == 0) {
+         EnvPrintRouter(theEnv, msg("werror"),
+               msg("\nNo arguments provided!\n"));
+         return 0;
       }
-      args.insert(it, mod->getModuleIdentifier());
-      return ee->runFunctionAsMain(entryFunction, args, envp);
-   }
-   int ClangCompiler::executeMain(llvm::Module* mod, std::vector<std::string>& args) {
-      return execute(mod, args, "main"); 
+      void* mainAddr = (void*) (intptr_t) getExecutablePath;
+      llvm::sys::Path path = getExecutablePath(getArgv0());
+      TextDiagnosticPrinter* diagClient = 
+         new TextDiagnosticPrinter(llvm::errs(), DiagnosticOptions());
+      IntrusiveRefCntPtr<DiagnosticIDs> diagID(new DiagnosticIDs());
+      DiagnosticsEngine diags (diagID, diagClient);
+      Driver theDriver(path.str(), llvm::sys::getDefaultTargetTriple(),
+            "a.out", false, diags);
+      theDriver.setTitle("rampancy knowledge compiler");
+
+      llvm::SmallVector<const char*, 16> args;
+      void* mfptr = GetValue(multifield);
+      int mfEnd = GetDOEnd(multifield);
+      for(int i = GetDOBegin(multifield); i <= mfEnd; ++i) {
+         int mfType = GetMFType(mfptr, i);
+         if(mfType == STRING || mfType == SYMBOL) {
+            //we need to do this to prevent the CLIPS gc from crashing the
+            //party. 
+            std::string argument(ValueToString(GetMFValue(mfptr, i)));
+            args.push_back(argument.c_str());
+         } else if(mfType == INTEGER) {
+            char* arg = CharBuffer(256);
+            sprintf(arg, "%lld", ValueToLong(GetMFValue(mfptr, i))); 
+            std::string argument(arg);
+            free(arg);
+            args.push_back(argument.c_str());
+         } else if(mfType == FLOAT) {
+            char* arg = CharBuffer(256);
+            sprintf(arg, "%f", ValueToDouble(GetMFValue(mfptr, i)));
+            std::string argument(arg);
+            free(arg);
+            args.push_back(argument.c_str());
+         } else {
+            EnvPrintRouter(theEnv, msg("werror"),
+                  msg("\nFound an argument that was not a string, symbol, or number\n"));
+            return 0;
+         }
+      }
+      //we need to iterate through the multifield
+      //we have our 
+      args.push_back("-fsyntax-only");
+      OwningPtr<Compilation> c(theDriver.BuildCompilation(args));
+      if(!c) return 0;
+
+      const driver::JobList &jobs = c->getJobs();
+      if(jobs.size() != 1 || !isa<driver::Command>(*jobs.begin())) {
+         SmallString<256> msg;
+         llvm::raw_svector_ostream os(msg);
+         c->PrintJob(os, c->getJobs(), "; ", true);
+         diags.Report(diag::err_fe_expected_compiler_job) << os.str();
+         return 0;
+      }
+
+      const driver::Command *cmd = cast<driver::Command>(*jobs.begin());
+      if(llvm::StringRef(cmd->getCreator().getName()) != "clang") {
+         diags.Report(diag::err_fe_expected_clang_command);
+         return 0;
+      }
+
+      const driver::ArgStringList &ccArgs = cmd->getArguments();
+      OwningPtr<CompilerInvocation> ci(new CompilerInvocation);
+      CompilerInvocation::CreateFromArgs(*ci, 
+            const_cast<const char **>(ccArgs.data()),
+            const_cast<const char **>(ccArgs.data()) +
+            ccArgs.size(),
+            diags);
+      if(ci->getHeaderSearchOpts().Verbose) {
+         llvm::errs() << "rampancy compilation:\n";
+         llvm::errs() << "  clang invocation:\n";
+         c->PrintJob(llvm::errs(), c->getJobs(), "\n", true);
+         llvm::errs() << "\n";
+      }
+
+      CompilerInstance clang;
+      clang.setInvocation(ci.take());
+
+      clang.createDiagnostics(int(ccArgs.size()), 
+            const_cast<char**>(ccArgs.data()));
+      if(!clang.hasDiagnostics())
+         return 0;
+      if(clang.getHeaderSearchOpts().UseBuiltinIncludes &&
+            clang.getHeaderSearchOpts().ResourceDir.empty()) {
+         clang.getHeaderSearchOpts().ResourceDir = 
+            CompilerInvocation::GetResourcesPath(getArgv0(), mainAddr);
+      }
+
+      OwningPtr<CodeGenAction> act(new EmitLLVMOnlyAction(getContext()));
+      if(!clang.ExecuteAction(*act))
+         return 0;
+      return act->takeModule();
    }
 
-   llvm::Module* ClangCompiler::compile(int argc, const char **argv, bool constructKnowledge) {
-      resetKnowledgeBuilder();   
-
+   llvm::Module* ClangCompiler::compile(int argc, char **argv) {
       void* mainAddr = (void*) (intptr_t) getExecutablePath;
       llvm::sys::Path path = getExecutablePath(argv[0]);
       TextDiagnosticPrinter* diagClient = new TextDiagnosticPrinter(llvm::errs(), DiagnosticOptions());
@@ -105,19 +184,23 @@ namespace rampancy {
             CompilerInvocation::GetResourcesPath(argv[0], mainAddr);
       }
 
-      OwningPtr<CodeGenAction> act(new EmitLLVMOnlyAction());
+      OwningPtr<CodeGenAction> act(new EmitLLVMOnlyAction(getContext()));
       if(!clang.ExecuteAction(*act))
          return 0;
-      llvm::Module* module = act->takeModule();
-      if(module && constructKnowledge) {
-         builder->route(module);
-         std::string result = getCompleteKnowledgeString();
-         env->makeInstances((char*)result.c_str());
-      } 
-      return module;
+      return act->takeModule();
    }
-   std::string ClangCompiler::getCompleteKnowledgeString() {
-      return builder->getInstancesAsString();
+   llvm::Module* ClangCompiler::interpret() {
+      EnvPrintRouter(getEnvironment()->getEnvironment(), 
+            msg("werror"),
+            msg("\nInterpreting code isn't supported with clang!\n"));
+      return 0;
    }
+   llvm::Module* ClangCompiler::interpret(llvm::StringRef input) {
+      EnvPrintRouter(getEnvironment()->getEnvironment(), 
+            msg("werror"),
+            msg("\nInterpreting code isn't supported with clang!\n"));
+      return 0;
+   }
+#undef msg
 }
 
