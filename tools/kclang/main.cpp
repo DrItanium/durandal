1a4ad60813c65eae62eb17c941d0f09fea4ab042
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-16 13:37:37 -0800

    Updated kclang to use the new compilation system.

--- tools/kclang/main.cpp
+++ tools/kclang/main.cpp
@@ -7,22 +7,42 @@
 #include "ExpertSystem/KnowledgeConstructionEngine.h"
 #include "rampancy/Compiler.h"
 #include "rampancy/ClangCompiler.h"
+#include "rampancy/Cortex.h"
 
 extern "C" {
    #include "clips.h"
 }
 
 int main(int argc, char** argv, char* const *envp) {
-   CLIPSEnvironment env;
-   env.batchStar((char*)"Init.clp");
-   env.eval((char*)"(set-current-module MAIN)");
-   rampancy::ClangCompiler compiler;
-   llvm::Module* mod = compiler.compile(argc, argv);
+   rampancy::ClangCompiler::argv0 = argv[0];
+   rampancy::Cortex* rampantCortex = rampancy::Cortex::getRampantCortex();
+   rampancy::CompilerRegistry* compilerRegistry = 
+      rampancy::CompilerRegistry::getCompilerRegistry();
+   PassRegistry &registry = *PassRegistry::getPassRegistry();
+   initializeCore(registry);
+   initializeScalarOpts(registry);
+   initializeVectorization(registry);
+   initializeIPO(registry);
+   initializeAnalysis(registry);
+   initializeIPA(registry);
+   initializeTransformUtils(registry);
+   initializeInstCombine(registry);
+   initializeInstrumentation(registry);
+   initializeTarget(registry);
+   rampancy::initializeClangCompilerPass(registry);
+   rampancy::ClangCompiler cc;
+   //set the 
+   CLIPSEnvironment* theEnv = rampantCortex->getEnvironment();
+   theEnv->batchStar("Init.clp");
+   llvm::Module* mod = cc.compile(argc, argv);
 	//we need to setup 
    if(mod) {
-      env.eval((char*)"(save-instances \"instances\" visible)");
+      llvm::StringRef name("clang");
+      rampantCortex->convertToKnowledge(name, mod);
+      theEnv->eval((char*)"(list-defmodules)");
+      theEnv->eval((char*)"(save-instances \"instances\" visible)");
    } else {
-      env.eval((char*)"(printout t \"ERROR: Something went wrong\" crlf)");
+      theEnv->eval((char*)"(printout t \"ERROR: Something went wrong\" crlf)");
    }
    llvm::llvm_shutdown();
 }
