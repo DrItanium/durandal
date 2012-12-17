f24de3d85f45cc05fbea4e2421ce952d6a74bc72
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-16 17:28:32 -0800

    Fixed the issue with compiling from CLIPS.
    I fixed it by creating my own argc set which is passed to compile(int argc,
    char** argv) in ClangCompiler.
    
    This not only makes the code cleaner, but also reduces the amount of
    duplication as well.
    
    The clips now fails in the exact same spot that kclang does.

--- lib/rampancy/RampancyClangCompiler.cpp
+++ lib/rampancy/RampancyClangCompiler.cpp
@@ -30,18 +30,9 @@ namespace rampancy {
                msg("\nNo arguments provided!\n"));
          return 0;
       }
-      void* mainAddr = (void*) (intptr_t) getExecutablePath;
-      llvm::sys::Path path = getExecutablePath(argv0);
-      TextDiagnosticPrinter* diagClient = 
-         new TextDiagnosticPrinter(llvm::errs(), DiagnosticOptions());
-      IntrusiveRefCntPtr<DiagnosticIDs> diagID(new DiagnosticIDs());
-      DiagnosticsEngine diags (diagID, diagClient);
-      Driver theDriver(path.str(), llvm::sys::getDefaultTargetTriple(),
-            "a.out", false, diags);
-      theDriver.setTitle("rampancy knowledge compiler");
-
-      llvm::SmallVector<const char*, 16> args;
-      args.push_back(argv0);
+      char** argv = (char**)calloc(1 + length, sizeof(char*)); 
+      int argc = 1 + length;
+      argv[0] = (char*)argv0;
       void* mfptr = GetValue(multifield);
       int mfEnd = GetDOEnd(multifield);
       for(int i = GetDOBegin(multifield); i <= mfEnd; ++i) {
@@ -50,76 +41,30 @@ namespace rampancy {
             //we need to do this to prevent the CLIPS gc from crashing the
             //party. 
             char* tmp = ValueToString(GetMFValue(mfptr, i));
-            std::string argument(tmp);
-            args.push_back(argument.c_str());
+            char* arg = (char*)calloc(strlen(tmp)+1, sizeof(char));
+            sprintf(arg, "%s", tmp);
+            argv[i] = arg;
          } else if(mfType == INTEGER) {
-            char* arg = CharBuffer(256);
+            char* arg = CharBuffer(128);
             sprintf(arg, "%lld", ValueToLong(GetMFValue(mfptr, i))); 
-            std::string argument(arg);
-            free(arg);
-            args.push_back(argument.c_str());
+            argv[i] = arg;
          } else if(mfType == FLOAT) {
-            char* arg = CharBuffer(256);
+            char* arg = CharBuffer(128);
             sprintf(arg, "%f", ValueToDouble(GetMFValue(mfptr, i)));
-            std::string argument(arg);
-            free(arg);
-            args.push_back(argument.c_str());
+            argv[i] = arg;
          } else {
             EnvPrintRouter(theEnv, msg("werror"),
                   msg("\nFound an argument that was not a string, symbol, or number\n"));
             return 0;
          }
       }
-      args.push_back("-fsyntax-only");
-      OwningPtr<Compilation> c(theDriver.BuildCompilation(args));
-      if(!c) return 0;
-
-      const driver::JobList &jobs = c->getJobs();
-      if(jobs.size() != 1 || !isa<driver::Command>(*jobs.begin())) {
-         SmallString<256> msg;
-         llvm::raw_svector_ostream os(msg);
-         c->PrintJob(os, c->getJobs(), "; ", true);
-         diags.Report(diag::err_fe_expected_compiler_job) << os.str();
-         return 0;
+      //we just create argv ourselves
+      llvm::Module* result = compile(argc, argv);
+      for(int i = 1; i < argc; ++i) {
+         free(argv[i]);
       }
-
-      const driver::Command *cmd = cast<driver::Command>(*jobs.begin());
-      if(llvm::StringRef(cmd->getCreator().getName()) != "clang") {
-         diags.Report(diag::err_fe_expected_clang_command);
-         return 0;
-      }
-
-      const driver::ArgStringList &ccArgs = cmd->getArguments();
-      OwningPtr<CompilerInvocation> ci(new CompilerInvocation);
-      CompilerInvocation::CreateFromArgs(*ci, 
-            const_cast<const char **>(ccArgs.data()),
-            const_cast<const char **>(ccArgs.data()) +
-            ccArgs.size(),
-            diags);
-      if(ci->getHeaderSearchOpts().Verbose) {
-         llvm::errs() << "rampancy compilation:\n";
-         llvm::errs() << "  clang invocation:\n";
-         c->PrintJob(llvm::errs(), c->getJobs(), "\n", true);
-         llvm::errs() << "\n";
-      }
-
-      CompilerInstance clang;
-      clang.setInvocation(ci.take());
-
-      clang.createDiagnostics(int(ccArgs.size()), 
-            const_cast<char**>(ccArgs.data()));
-      if(!clang.hasDiagnostics())
-         return 0;
-      if(clang.getHeaderSearchOpts().UseBuiltinIncludes &&
-            clang.getHeaderSearchOpts().ResourceDir.empty()) {
-         clang.getHeaderSearchOpts().ResourceDir = 
-            CompilerInvocation::GetResourcesPath(argv0, mainAddr);
-      }
-
-      OwningPtr<CodeGenAction> act(new EmitLLVMOnlyAction(getContext()));
-      if(!clang.ExecuteAction(*act))
-         return 0;
-      return act->takeModule();
+      free(argv);
+      return result;
    }
 
    llvm::Module* ClangCompiler::compile(int argc, char **argv) {
@@ -134,7 +79,6 @@ namespace rampancy {
 
       llvm::SmallVector<const char*, 16> args(argv, argv + argc);
       args.push_back("-fsyntax-only");
-      //args.push_back("-S");
       OwningPtr<Compilation> c(theDriver.BuildCompilation(args));
       if(!c) return 0;
 
@@ -208,35 +152,4 @@ namespace rampancy {
          "dynamic clang for use with CLIPS", 
          false,
          false);
-
-   //void* initializeClangCompilerPassOnce(llvm::PassRegistry& registry) {
-   //   llvm::PassInfo *pi = new PassInfo("dynamic clang for use with CLIPS",
-   //         "clang-dynamic-compiler", &ClangCompiler::ID,
-   //         PassInfo::NormalCtor_t(callDefaultCtor<ClangCompiler>), false, 
-   //         false);
-   //   registry.registerPass(*pi, true);
-   //   return pi;
-   //}
-   //void initializeClangCompilerPass(llvm::PassRegistry& registry) {
-   //   //initializeClangCompilerPassOnce is the input
-   //   static volatile llvm::sys::cas_flag initialized = 0;
-   //   llvm::sys::cas_flag old_val = sys::CompareAndSwap(&initialized, 1, 0);
-   //   if(old_val == 0) {
-   //      initializeClangCompilerPassOnce(registry);
-   //      llvm::sys::MemoryFence();
-   //      TsanIgnoreWritesBegin();
-   //      TsanHappensBefore(&initialized);
-   //      initialized = 2;
-   //      TsanIgnoreWritesEnd();
-   //   } else {
-   //      llvm::sys::cas_flag tmp = initialized;
-   //      llvm::sys::MemoryFence();
-   //      while(tmp != 2) {
-   //         tmp = initialized;
-   //         llvm::sys::MemoryFence();
-   //      }
-   //   }
-   //   TsanHappensAfter(&initialized);
-   //}
-
 }
