50597c88d52168ac740902e0bcc185141c43697d
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-21 15:00:16 -0800

    Added the implementation details for IndirectPass

---
+++ lib/indirect/IndirectPass.cpp
@@ -0,0 +1,16 @@
+#include "indirect/IndirectPass.h"
+
+namespace indirect {
+   IndirectPass::IndirectPass() { }
+   IndirectPass::~IndirectPass() { }
+
+   void IndirectPass::setIndirectPassHeader(IndirectPassHeader* hdr) {
+      header = hdr;
+   }
+   IndirectPassHeader* IndirectPass::getIndirectPassHeader() {
+      return header;
+   }
+   void IndirectPass::applyIndirectAnalysisUsage(llvm::AnalysisUsage& usage) {
+      (*header)(usage);
+   }
+}
