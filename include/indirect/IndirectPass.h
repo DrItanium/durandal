67174f61e99e6d4254019c7567775b09f295e74d
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-21 13:56:25 -0800

    Added the IndirectPass header
    The indirect pass takes the extended functionality of an IndirectPassHeader and
    provides all of the functionality to make it really easy to add to a new pass
    type.

---
+++ include/indirect/IndirectPass.h
@@ -0,0 +1,23 @@
+#ifndef _indirect_pass_h
+#define _indirect_pass_h
+#include "indirect/IndirectPassHeader.h"
+#include "llvm/Pass.h"
+
+/* This class is meant to interface with the different template pass classes to
+ * provide the indirection functionality without having to reinvent the wheel
+ * for each pass. 
+ */
+
+namespace indirect {
+   class IndirectPass {
+      private:
+         IndirectPassHeader* header;
+      public:
+         IndirectPass();
+         ~IndirectPass();
+         void setIndirectPassHeader(IndirectPassHeader* header);
+         IndirectPassHeader* getIndirectPassHeader();
+         void applyIndirectAnalysisUsage(llvm::AnalysisUsage& usage);
+   };
+}
+#endif
