c621d4723d9c41edca09c112460cbf8672c95258
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-21 13:49:27 -0800

    Added the header for IndirectPassGenerator
    The IndirectPassGenerator is a way to generate base types for a given template
    set. The base class IndirectPassGeneratorBase has a single method createPass
    which returns an llvm::Pass* and takes in a IndirectPassHeader*. It is up to
    subclasses to implement the rest of the functionality.
    
    The subclass IndirectPassGenerator is a class that one defines the different
    pass templates in. The rest of the functionality is handled automatically by
    the class itself. One uses the PassTypeNotSupported class to mark a given pass
    type as not supported.

---
+++ include/indirect/IndirectPassGenerator.h
@@ -0,0 +1,27 @@
+#ifndef _indirect_pass_generator_h
+#define _indirect_pass_generator_h
+#include "llvm/Pass.h"
+#include "indirect/IndirectPassHeader.h"
+#include "indirect/IndirectPasses.h"
+namespace indirect {
+   class IndirectPassGeneratorBase {
+      public:
+         virtual llvm::Pass* createPass(IndirectPassHeader* header) = 0;
+   };
+
+   template<typename MP, typename FP, typename BBP, typename LP, typename RP, 
+      typename MFP, typename CGSCCP> 
+         class IndirectPassGeneratorTemplate : public IndirectPassGeneratorBase {
+            public:
+               using indirect::IndirectPassGeneratorBase::createPass;
+               virtual llvm::Pass* createPass(IndirectPassHeader* header);
+               IndirectModulePass* generateBaseModulePass(char& id);
+               IndirectFunctionPass* generateBaseFunctionPass(char& id);
+               IndirectBasicBlockPass* generateBaseBasicBlockFunctionPass(char& id);
+               IndirectLoopPass* generateBaseLoopFunctionPass(char& id);
+               IndirectMachineFunctionPass* generateBaseMachineFunctionPass(char& id);
+               IndirectRegionPass* generateBaseRegionPass(char& id);
+               IndirectCallGraphSCCPass* generateBaseCallGraphSCCPass(char& id);
+         };
+}
+#endif
