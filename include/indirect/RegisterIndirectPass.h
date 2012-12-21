9b1be2f23f790e20fe5913f078eeffd4431bebcb
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-21 12:38:04 -0800

    Added the RegisterIndirectPass header
    RegisterIndirectPass is like RegisterPass but for indirect passes. It does not
    provide a default constructor because I can't figure out how to close over
    variables yet make the resulting function a function-pointer instead of a
    member-function pointer.

---
+++ include/indirect/RegisterIndirectPass.h
@@ -0,0 +1,30 @@
+#ifndef _register_indirect_pass_h
+#define _register_indirect_pass_h
+/* IndirectRegisterPass is a class used to register indirect passes in a fashion
+ * similar to how llvm::RegisterPass works.
+ */
+#include "llvm/Pass.h" 
+#include "indirect/IndirectPassRegistry.h"
+
+namespace indirect {
+   class RegisterIndirectPass : public llvm::PassInfo {
+      public:
+         RegisterIndirectPass(const char* passArg, const char* passName, 
+               bool cfgOnly = false, bool isAnalysis = false) : PassInfo(passName,
+                  passArg, IndirectPassRegistry::getUniqueId(passArg), 
+                  /* we can't provide a default constructor because it's necessary
+                   * to dynamically acquire the unique id of the indirect pass.
+                   * Since LLVM was not designed to do this I have to work within
+                   * it's confines. I have no intention of introducing changes to
+                   * a system that is already working quite well. 
+                   *
+                   * Thus there is no default constructor for indirect passes.
+                   * There are ones for the templates though.
+                   */
+                  0, cfgOnly, isAnalysis) {
+                  PassRegistry::getPassRegistry()->registerPass(*this);
+               }
+   };
+}
+
+#endif
