2dbb09d70d70a6f34dd9e475b7496bca34ae6327
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-22 14:52:01 -0800

    Moved the template function code into IndirectPassRegistry.h

--- lib/indirect/IndirectPassRegistry.cpp
+++ lib/indirect/IndirectPassRegistry.cpp
@@ -7,10 +7,6 @@ namespace indirect {
    IndirectPassRegistry* IndirectPassRegistry::getIndirectPassRegistry() {
       return &*indirectPassRegistryObject;
    }
-   template<typename PassGeneratorClass> 
-      IndirectPassGeneratorBase* IndirectPassRegistry::buildPassGenerator() {
-         return new PassGeneratorClass();
-      }
    const void* IndirectPassRegistry::getUniqueId(const char* name) {
       return (getIndirectPassRegistry()->getIndirectPassInfo(name))->getTypeInfo();
    }
@@ -28,24 +24,6 @@ namespace indirect {
 
    }
 
-   template<typename PassGeneratorClass>
-      void IndirectPassRegistry::registerPassGenerator(char* name) {
-         registerPassGenerator<PassGeneratorClass>(
-               llvm::StringRef((const char*)name));
-      }
-   template<typename PassGeneratorClass>
-      void IndirectPassRegistry::registerPassGenerator(const char* name) {
-         registerPassGenerator<PassGeneratorClass>(
-               llvm::StringRef(name));
-      }
-   template<typename PassGeneratorClass>
-      void IndirectPassRegistry::registerPassGenerator(llvm::StringRef name) {
-         assert((registeredPassGenerators.find(name) == 
-                  registeredPassGenerators.end()) && 
-               "Pass generator already registered!");
-         registeredPassGenerators[name] = 
-            IndirectPassRegistry::buildPassGenerator<PassGeneratorClass>();
-      }
    void IndirectPassRegistry::registerIndirectPassHeader(
          IndirectPassHeader* header) {
       llvm::StringRef passName(header->getPassName());
