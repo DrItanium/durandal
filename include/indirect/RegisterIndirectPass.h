c6caaea50c8033774c011387e318192417cdbbbc
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-21 19:40:57 -0800

    Moved the commented out code in RegisterIndirectPass.h into the cpp file

--- include/indirect/RegisterIndirectPass.h
+++ include/indirect/RegisterIndirectPass.h
@@ -13,20 +13,6 @@ namespace indirect {
          RegisterIndirectPass(IndirectPassHeader* header);
          RegisterIndirectPass(const char* passArg, const char* passName, 
                bool cfgOnly = false, bool isAnalysis = false);
-         //: llvm::PassInfo(passName,
-          //        passArg, indirect::IndirectPassRegistry::getUniqueId(passArg), 
-                  /* we can't provide a default constructor because it's necessary
-                   * to dynamically acquire the unique id of the indirect pass.
-                   * Since LLVM was not designed to do this I have to work within
-                   * it's confines. I have no intention of introducing changes to
-                   * a system that is already working quite well. 
-                   *
-                   * Thus there is no default constructor for indirect passes.
-                   * There are ones for the templates though.
-                   */
-           //       0, cfgOnly, isAnalysis) {
-            //      llvm::PassRegistry::getPassRegistry()->registerPass(*this);
-             //  }
                   
    };
 }
