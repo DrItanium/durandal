733f71159186900a8964b5e2ef172ab7e2fb539b
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-22 13:56:17 -0800

    Added the Indirector header
    This header includes all of the other files that make up libindirect.
    Include this file to as a way to include all of libindirect.

---
+++ include/indirect/Indirector.h
@@ -0,0 +1,18 @@
+#ifndef _indirect_indirector_h
+#define _indirect_indirector_h
+
+/*
+ * This file is a wrapper over the other include files that make up
+ * libindirect. It is meant to make libindirect easy to use
+ */
+
+
+#include "indirect/IndirectAnalysisUsageDeclaration.h"
+#include "indirect/IndirectPasses.h"
+#include "indirect/IndirectPassGenerator.h"
+#include "indirect/IndirectPass.h"
+#include "indirect/IndirectPassHeader.h"
+#include "indirect/IndirectPassRegistry.h"
+#include "indirect/IndirectUniqueIdentifier.h"
+#include "indirect/RegisterIndirectPass.h"
+#endif
