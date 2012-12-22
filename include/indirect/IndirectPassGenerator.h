625a086b615c3e87c3e22d77d03fab5e35ee0890
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-22 14:49:51 -0800

    Moved the code in IndirectPassRegistry.cpp into the header
    This was necessary because the class is a template class and the template
    definitions have to be local to the class itself.

--- include/indirect/IndirectPassGenerator.h
+++ include/indirect/IndirectPassGenerator.h
@@ -14,15 +14,91 @@ namespace indirect {
          class IndirectPassGeneratorTemplate : public IndirectPassGeneratorBase {
             public:
                IndirectPassGeneratorTemplate() { }
+               IndirectCallGraphSCCPass* generateBaseCallGraphSCCPass(char& id) {
+                  return new CGSCCP(id);
+               }
+
+               IndirectRegionPass* generateBaseRegionPass(char& id) {
+                  return new RP(id);
+               }
+
+               IndirectMachineFunctionPass* generateBaseMachineFunctionPass(char& id) {
+                  return new MFP(id);
+               }
+
+               IndirectLoopPass* generateBaseLoopPass(char& id) {
+                  return new LP(id);
+               }
+
+               IndirectBasicBlockPass* generateBaseBasicBlockPass(char& id) {
+                  return new BBP(id);
+               }
+
+               IndirectFunctionPass* generateBaseFunctionPass(char& id) {
+                  return new FP(id);
+               }
+
+               IndirectModulePass* generateBaseModulePass(char& id) {
+                  return new MP(id);
+               }
                using indirect::IndirectPassGeneratorBase::createPass;
-               virtual llvm::Pass* createPass(IndirectPassHeader* header);
-               IndirectModulePass* generateBaseModulePass(char& id);
-               IndirectFunctionPass* generateBaseFunctionPass(char& id);
-               IndirectBasicBlockPass* generateBaseBasicBlockPass(char& id);
-               IndirectLoopPass* generateBaseLoopPass(char& id);
-               IndirectMachineFunctionPass* generateBaseMachineFunctionPass(char& id);
-               IndirectRegionPass* generateBaseRegionPass(char& id);
-               IndirectCallGraphSCCPass* generateBaseCallGraphSCCPass(char& id);
+
+               virtual llvm::Pass* createPass(IndirectPassHeader* header) {
+                  /*
+                   * the first thing to do is to determine what kind of a pass this is
+                   * supposed to be. 
+                   */
+                  char& id = *(header->getID());
+                  switch(header->getPassType()) {
+                     case IndirectPassHeader::Module: 
+                        {
+                           IndirectModulePass* pass = generateBaseModulePass(id);
+                           pass->setIndirectPassHeader(header);
+                           return pass;
+                        }
+                     case IndirectPassHeader::Function: 
+                        {
+                           IndirectFunctionPass* pass = generateBaseFunctionPass(id);
+                           pass->setIndirectPassHeader(header);
+                           return pass;
+                        }
+                     case IndirectPassHeader::BasicBlock:
+                        {
+                           IndirectBasicBlockPass* pass = 
+                              generateBaseBasicBlockPass(id);
+                           pass->setIndirectPassHeader(header);
+                           return pass;
+                        }
+                     case IndirectPassHeader::Loop:
+                        {
+                           IndirectLoopPass* pass = generateBaseLoopPass(id);
+                           pass->setIndirectPassHeader(header);
+                           return pass;
+                        }
+                     case IndirectPassHeader::Region:
+                        {
+                           IndirectRegionPass* pass = generateBaseRegionPass(id);
+                           pass->setIndirectPassHeader(header);
+                           return pass;
+                        }
+                     case IndirectPassHeader::MachineFunction:
+                        {
+                           IndirectMachineFunctionPass* pass = 
+                              generateBaseMachineFunctionPass(id);
+                           pass->setIndirectPassHeader(header);
+                           return pass;
+                        }
+                     case IndirectPassHeader::CallGraphSCC:
+                        {
+                           IndirectCallGraphSCCPass* pass = 
+                              generateBaseCallGraphSCCPass(id);
+                           pass->setIndirectPassHeader(header);
+                           return pass;
+                        }
+                     default: 
+                        return 0;
+                  }
+               }
          };
 }
 #endif
