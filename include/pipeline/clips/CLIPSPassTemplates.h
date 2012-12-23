50b234e5bd254a9e08acb6eb0213ddcbb7a61e93
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-22 19:02:03 -0800

    Added a header file containing the CLIPSTemplatePasses
    These template passes are templates used to provide the C++ logic necessary to
    run clips and load the corresponding clips passes.

---
+++ include/pipeline/clips/CLIPSPassTemplates.h
@@ -0,0 +1,53 @@
+#ifndef _clips_pass_templates.h
+#define _clips_pass_templates.h
+#include "indirect/IndirectPasses.h"
+#include "pipeline/clips/CLIPSPass.h"
+using namespace indirect;
+namespace pipeline {
+   namespace clips {
+      class CLIPSModulePass : public IndirectModulePass, public CLIPSPass {
+         public:
+            CLIPSModulePass(char& id);
+            using IndirectModulePass::runOnModule;
+            virtual bool runOnModule(llvm::Module& module);
+      };
+      class CLIPSFunctionPass : public IndirectFunctionPass, public CLIPSPass {
+         public:
+            CLIPSFunctionPass(char& id);
+            using IndirectFunctionPass::runOnFunction;
+            virtual bool runOnFunction(llvm::Function& fn);
+      };
+      class CLIPSBasicBlockPass : public IndirectBasicBlockPass, public CLIPSPass {
+         public:
+            CLIPSBasicBlockPass(char& id);
+            using IndirectBasicBlockPass::runOnBasicBlock;
+            virtual bool runOnBasicBlock(llvm::BasicBlock& bb);
+      };
+      class CLIPSLoopPass : public IndirectLoopPass, public CLIPSPass {
+         public:
+            CLIPSLoopPass(char& id);
+            using IndirectLoopPass::runOnLoop;
+            virtual bool runOnLoop(llvm::Loop* l, llvm::LPPassManager& lpm);
+      };
+      class CLIPSRegionPass : public IndirectRegionPass, public CLIPSPass {
+         public:
+            CLIPSRegionPass(char& id);
+            using IndirectRegionPass::runOnRegion;
+            virtual bool runOnRegion(llvm::Region* r, llvm::RGPassManager& rg);
+      };
+      class CLIPSMachineFunctionPass : public IndirectMachineFunctionPass, public CLIPSPass {
+         public:
+            CLIPSMachineFunctionPass(char& id);
+            using IndirectMachineFunctionPass::runOnMachineFunction;
+            virtual bool runOnMachineFunction(llvm::MachineFunction& mf);
+      };
+      class CLIPSCallGraphSCCPass : public IndirectCallGraphSCCPass, public CLIPSPass {
+         public:
+            CLIPSCallGraphSCCPass(char& id);
+            using IndirectCallGraphSCCPass::runOnSCC;
+            virtual bool runOnSCC(llvm::CallGraphSCC& scc);
+      }
+   }
+}
+
+#endif
