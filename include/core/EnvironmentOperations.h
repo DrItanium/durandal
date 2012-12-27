603da09432c248b7b512119ec30c0aa9741c2eda
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-26 19:00:08 -0800

    Fixed a typo in one of the function names in EnvironmentOperations.h

--- include/core/EnvironmentOperations.h
+++ include/core/EnvironmentOperations.h
@@ -30,7 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 extern void EnvironmentOperationsDefinitions(void* theEnv);
 extern void EnvironmentDestroy(void* theEnv);
 extern void* GetCurrentlyExecutingEnvironment(void* theEnv);
-extern void* IsCurrentlExecutingEnvironment(void* theEnv);
+extern void* IsCurrentlyExecutingEnvironment(void* theEnv);
 extern void* ToPointer(void* theEnv);
 extern void RunEnvironment(void* theEnv);
 extern void EnvironmentEval(void* theEnv, DATA_OBJECT_PTR ret);
