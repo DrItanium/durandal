57fd709334b2807cbc32465f4687c908ca7bf882
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-01-09 23:10:00 -0800

    Enabled JSEdgeRemoval and the Makefile

--- lib/passes/JSEdgeRemoval.cpp
+++ lib/passes/JSEdgeRemoval.cpp
@@ -1,5 +1,4 @@
-#include "JSEdgeRemoval.h"
+#include "passes/JSEdgeRemoval.h"
 
 char JSEdgeRemoval::ID = 0;
 static RegisterPass<JSEdgeRemoval> js("jsedge", "JS Edge Removal", false, false);
-INITIALIZE_PASS(JSEdgeRemoval, "jsedge", "JS Edge Removal", false, false)
