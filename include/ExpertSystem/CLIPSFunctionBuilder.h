29827b9c236ee3838d1d08d4e6db73c720f03eba
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-01-12 12:30:51 -0800

    Reformatted CLIPSFunctionBuilder.h

--- include/ExpertSystem/CLIPSFunctionBuilder.h
+++ include/ExpertSystem/CLIPSFunctionBuilder.h
@@ -3,13 +3,13 @@
 #include "ExpertSystem/CLIPSConstantBuilder.h"
 
 using namespace llvm;
-   class CLIPSFunctionBuilder : public CLIPSGlobalValueBuilder {
-      public:
-         CLIPSFunctionBuilder(std::string nm, FunctionNamer& namer);
-         void setCallingConvention(CallingConv::ID id);
-         void setAttributes(const AttributeSet& attr);
-         void addFields(Function* fn, KnowledgeConstructor* kc, bool traverseBlocks = false);
-         void build(Function* fn, KnowledgeConstructor* kc, bool traverseBlocks = false);
-   };
+class CLIPSFunctionBuilder : public CLIPSGlobalValueBuilder {
+   public:
+      CLIPSFunctionBuilder(std::string nm, FunctionNamer& namer);
+      void setCallingConvention(CallingConv::ID id);
+      void setAttributes(const AttributeSet& attr);
+      void addFields(Function* fn, KnowledgeConstructor* kc, bool traverseBlocks = false);
+      void build(Function* fn, KnowledgeConstructor* kc, bool traverseBlocks = false);
+};
 
 #endif
