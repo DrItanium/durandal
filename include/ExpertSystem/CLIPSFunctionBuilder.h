ef651adddd2dc1c39bf3a52ef58b2891b046f9f0
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-01-12 12:29:57 -0800

    added the setAttributes method to CLIPSFunctionBuilder.h
    This method is meant to decompose an AttributeSet object into a set of
    symbols that are stored in the function's Attributes multislot

--- include/ExpertSystem/CLIPSFunctionBuilder.h
+++ include/ExpertSystem/CLIPSFunctionBuilder.h
@@ -7,6 +7,7 @@ using namespace llvm;
       public:
          CLIPSFunctionBuilder(std::string nm, FunctionNamer& namer);
          void setCallingConvention(CallingConv::ID id);
+         void setAttributes(const AttributeSet& attr);
          void addFields(Function* fn, KnowledgeConstructor* kc, bool traverseBlocks = false);
          void build(Function* fn, KnowledgeConstructor* kc, bool traverseBlocks = false);
    };
