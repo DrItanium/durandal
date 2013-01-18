eeb6a29a7d15aec5d8b2faaebe049759c0038e26
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-01-17 20:52:19 -0800

    Commented out the !isBasicBlockPass check in BasicBlock population logic
    I thought about it and there must be a better way to do this through a readonly
    mechanism in C++.

--- include/knowledge/BasicBlockKnowledgeRepresentation.h
+++ include/knowledge/BasicBlockKnowledgeRepresentation.h
@@ -31,31 +31,31 @@ namespace knowledge {
             }
             //only provide predecessors and successors if we aren't in a basic
             //block pass
-            if(!kc->isBasicBlockPass()) {
-               pred_iterator pi = pred_begin(bb), 
-                             pe = pred_end(bb);
-               if(pi != pe) {
-                  krb->openMultifield("Predecessors");
-                  for(; pi != pe; ++pi) {
-                     BasicBlock* pred = *pi;
-                     //we can fixup the parents at a later time if necessary
-                     krb->appendValue(Route(pred, kc, parent));
-                  }
-                  krb->closeMultifield();
+            //if(!kc->isBasicBlockPass()) {
+            pred_iterator pi = pred_begin(bb), 
+                          pe = pred_end(bb);
+            if(pi != pe) {
+               krb->openMultifield("Predecessors");
+               for(; pi != pe; ++pi) {
+                  BasicBlock* pred = *pi;
+                  //we can fixup the parents at a later time if necessary
+                  krb->appendValue(Route(pred, kc, parent));
                }
-               succ_iterator si = succ_begin(bb), 
-                             se = succ_end(bb);
-               if(si != se) {
-                  krb->openMultifield("Successors");
-                  for(; si != se; ++si) {
-                     BasicBlock* succ = *si;
-                     PointerAddress pa = (PointerAddress)succ;
-                     //we can fixup the parents at a later time if necessary
-                     krb->appendValue(Route(succ, kc, parent));
-                  }
-                  krb->closeMultifield();
-               }
+               krb->closeMultifield();
             }
+            succ_iterator si = succ_begin(bb), 
+                          se = succ_end(bb);
+            if(si != se) {
+               krb->openMultifield("Successors");
+               for(; si != se; ++si) {
+                  BasicBlock* succ = *si;
+                  PointerAddress pa = (PointerAddress)succ;
+                  //we can fixup the parents at a later time if necessary
+                  krb->appendValue(Route(succ, kc, parent));
+               }
+               krb->closeMultifield();
+            }
+            //}
             if(!bb->empty()) {
                std::string tmp;
                raw_string_ostream data(tmp);
