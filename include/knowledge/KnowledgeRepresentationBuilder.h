f1638bdc75c9d74c355ca9441378e48842020326
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-01-17 19:26:03 -0800

    Added the header for the rewritten CLIPSObjectBuilder class
    This new class is known as KnowledgeRepresentationBuilder because nothing
    inherits from it in the new design. This class is still responsible for
    building an instance string. Some of the functions have been renamed to be more
    meaningful as well. For instance, openField and closeField are now known as
    openMultifield and closeMultifield.
    
    I also change the data fields to be pointers to data items to allow the
    KnowledgeRepresentationBuilder to exist separately of a routing chain. While
    there is no real reason for this, it does make my life easier when debugging.

---
+++ include/knowledge/KnowledgeRepresentationBuilder.h
@@ -0,0 +1,60 @@
+#ifndef _knowledge_representation_builder_h
+#define _knowledge_representation_builder_h
+#include "llvm/Support/raw_ostream.h"
+#include "knowledge/Types.h"
+namespace knowledge {
+   class KnowledgeRepresentationBuilder {
+      bool opened;
+      bool closed;
+      bool in_multifield;
+      llvm::raw_string_ostream* stream;
+      std::string* name;
+      std::string* type;
+      std::string* parent;
+      std::string* tmp;
+      PointerAddress ptr;
+      public:
+      KnowledgeRepresentationBuilder(std::string& n, std::string& t, 
+            PointerAddress p, const char* parent);
+      ~KnowledgeRepresentationBuilder();
+      std::string getCompletedString();
+      bool isOpen() { return opened; }
+      bool isClosed() { return closed; }
+      bool inMultifield() { return in_multifield; }
+      void addField(const char* n, unsigned v);
+      void addField(const char* n, PointerAddress v); 
+      void addField(const char* n, int v); 
+      void addField(const char* n, float v);
+      void addField(const char* n, double v); 
+      void addField(const char* n, char* v); 
+      void addField(const char* n, const char* v); 
+      void addField(const char* n, std::string v); 
+		void addField(const char* n, llvm::StringRef ref);
+      void addField(const char* n, bool v); 
+      void addField(const char* n, uint64_t v);
+      void addField(const char* n, int64_t v); 
+      void addStringField(const char* n, const std::string& str);
+		void addTrueField(const char* n);
+		void addFalseField(const char* n);
+		void openMultifield(const char* name);
+		void appendValue(unsigned v);
+		void appendValue(PointerAddress v);
+		void appendValue(int v);
+		void appendValue(float v);
+		void appendValue(double v);
+		void appendValue(char* v);
+		void appendValue(const char* v);
+		void appendValue(std::string v);
+		void appendValue(llvm::StringRef ref);
+		void appendValue(bool v);
+		void appendValue(uint64_t v);
+		void appendValue(int64_t v);
+		void appendTrue();
+		void appendFalse();
+		void closeMultifield();
+      void open();
+      void close();
+   };
+}
+
+#endif
