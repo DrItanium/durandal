7949b40bc6b38d615e2dda74b7fd12022246402d
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-05-25 11:32:02 -0700

    Moved old code to Engine.cc

--- lib/knowledge/Engine.cc
+++ lib/knowledge/Engine.cc
@@ -15,9 +15,10 @@ namespace knowledge {
 void* makeInstance(void* theEnv, const std::string& str) {
 	void* result = EnvMakeInstance(theEnv, str.c_str());
 	if (result == NULL) {
-		llvm::raw_string_ostream msg;
-		msg << "Couldn't create instance from " << str.str();
-		llvm::report_fatal_error(msg.str())
+		std::string tmp;
+		llvm::raw_string_ostream msg(tmp);
+		msg << "Couldn't create instance from " << str;
+		llvm::report_fatal_error(msg.str());
 	} 
 	return result;
 }
@@ -26,19 +27,20 @@ void booleanField(llvm::raw_string_ostream& str, const std::string& name, bool v
 		str << FIELD(name, "TRUE");
 	}
 }
+template<class C> 
+void* getInstanceName(void* theEnv, C* instance) {
+	return EnvAddSymbol(theEnv, 
+			EnvGetInstanceName(theEnv, 
+				dispatch(theEnv, instance)));
+}
 template<class T>
 void setParent(void* theEnv, T* target)
 {
 	DATA_OBJECT wrapper;
+	void* instanceName = getInstanceName(theEnv, target->getParent());
 	SetType(wrapper, INSTANCE_NAME);
-	SetValue(wrapper, EnvAddSymbol(theEnv,
-				EnvGetInstanceName(theEnv,
-					convert(theEnv, target->getParent()))));
+	SetValue(wrapper, instanceName);
 	EnvDirectPutSlot(theEnv, GetNativeInstance(theEnv, target), "parent", &wrapper);
-}
-template<class C> 
-void* getInstanceName(void* theEnv, C* instance) {
-	return EnvAddSymbol(theEnv, EnvGetInstanceName(theEnv, convert(theEnv, instance)));
 }
 void directPutMultifield(void* theEnv, void* nativeInstance,
 		const std::string& slotName, void* multifieldData, int multifieldBegin,
@@ -50,19 +52,6 @@ void directPutMultifield(void* theEnv, void* nativeInstance,
 	SetDOEnd(wrapper, multifieldEnd);
 	EnvDirectPutSlot(theEnv, GetNativeInstance(theEnv, nativeInstance),
 			slotName.c_str(), &wrapper);
-}
-template<class C>
-void directPutMultifieldInstanceName(void* theEnv, 
-		C* rawInstance, const std::string& slotName) {
-	void* mf = EnvCreateMultifield(theEnv, rawInstance->size());
-	int index = 1;
-	for(typename C::iterator it = rawInstance->begin(); 
-			it != rawInstance->end(); 
-			++it, ++index) {
-		SetMFType(mf, index, INSTANCE_NAME);
-		SetMFValue(mf, index, getInstanceName(theEnv, *it));
-	}
-	directPutMultifield(theEnv, rawInstance, slotName, mf, 1, index - 1);
 }
 
 buildInstanceHeader(llvm::Module) {
@@ -78,23 +67,34 @@ buildInstanceHeader(llvm::BasicBlock) {
 	booleanField(str, "has-address-taken", data->hasAddressTaken());
 }
 populateInstanceHeader(llvm::BasicBlock) {
+	int index = 0;
 	setParent(theEnv, data);
 	if (data->size() > 0) {
-		directPutMultifieldInstanceName(theEnv, data, "contents");
+		void* mf = EnvCreateMultifield(theEnv, data->size());
+		index = 1;
+		for(llvm::BasicBlock::iterator it = data->begin();
+				it != data->end();
+				++it, ++index) {
+			llvm::Instruction& inst = *it;
+			SetMFType(mf, index, INSTANCE_NAME);
+			SetMFValue(mf, index, getInstanceName(theEnv, &inst));
+		}
+		directPutMultifield(theEnv, data, "contents", mf, 1, index - 1);
 	}
-	llvm::SmallVector<llvm::BasicBlock*, 8> predecessors(pred_begin(data), pred_end(data))
-		void *predMultifield = EnvCreateMultifield(theEnv, predecessors->size());
+	llvm::SmallVector<llvm::BasicBlock*, 8> predecessors(pred_begin(data), pred_end(data));
+	void *predMultifield = EnvCreateMultifield(theEnv, predecessors.size());
 	index = 1;
 	for (llvm::SmallVector<llvm::BasicBlock*,8>::iterator it = predecessors.begin();
 			it != predecessors.end(); ++it, ++index) {
 		SetMFType(predMultifield, index, INSTANCE_NAME);
 		SetMFValue(predMultifield, index, getInstanceName(theEnv, *it));
 	}
-	directPutMultifield(theEnv, data, "predecessors", predMultifield);
+	directPutMultifield(theEnv, data, "predecessors", predMultifield, 
+			1, predecessors.size());
 
 	llvm::SmallVector<llvm::BasicBlock*, 8> successors(succ_begin(data), succ_end(data));
-	void *succMultifield = EnvCreateMultifield(theEnv, successors->size());
-	void *producesMultifield = EnvCreateMultifield(theEnv, successors->size());
+	void *succMultifield = EnvCreateMultifield(theEnv, successors.size());
+	void *producesMultifield = EnvCreateMultifield(theEnv, successors.size());
 	index = 1;
 	for (llvm::SmallVector<llvm::BasicBlock*, 8>::iterator it = successors.begin(); 
 			it != successors.end(); ++it, ++index) {
@@ -104,15 +104,17 @@ populateInstanceHeader(llvm::BasicBlock) {
 		SetMFType(succMultifield, index, INSTANCE_NAME);
 		SetMFValue(succMultifield, index, result);
 	}
-	directPutMultifield(theEnv, data, "successors", succMultifield);
-	directPutMultifield(theEnv, data, "produces", producesMultifield);
+	directPutMultifield(theEnv, data, "successors", succMultifield, 
+			1, successors.size());
+	directPutMultifield(theEnv, data, "produces", producesMultifield, 
+			1, successors.size());
 }
 
 buildInstanceHeader(llvm::Argument) {
-	str << FIELD("index", arg->getArgNo());
+	str << FIELD("index", data->getArgNo());
 }
 populateInstanceHeader(llvm::Argument) {
-	setParent(theEnv, arg);
+	setParent(theEnv, data);
 }
 
 
